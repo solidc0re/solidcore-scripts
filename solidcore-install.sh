138f270cef70f1071eebf34ff4c6ae4cfa3b93c3
solidc0re <140627847+solidc0re@users.noreply.github.com>
2023-08-26 19:20:22 +0100

    Update solidcore-install.sh

--- solidcore-install.sh
+++ solidcore-install.sh
@@ -63,22 +63,12 @@ typeit() {
  local IFS=''
  while read -n1 c; do
  echo -n "$c"
- sleep 0.1
+ sleep 0.05
  done <<< "$1"
+ echo
 }
 
-echo "                  888 d8b      888                                   "       
-echo "                  888 Y8P      888                                   " 
-echo "                  888          888                                   "
-echo ".d8888b   .d88b.  888 888  .d88888  .d8888b  .d88b.  888d888  .d88b. " 
-echo "88K      d88""88b 888 888 d88" 888 d88P"    d88""88b 888P"   d8P  Y8b" 
-echo ""Y8888b. 888  888 888 888 888  888 888      888  888 888     88888888"
-echo "     X88 Y88..88P 888 888 Y88b 888 Y88b.    Y88..88P 888     Y8b.    " 
-echo " 88888P'  "echo"Y88P"echo"  888 888  "echo"Y88888  "echo"Y8888P  "echo"Y88P"echo"  888      "echo"Y8888 "
-echo
-echo
 
-sleep 3
 echo
 echo
 typeit "Welcome to solidcore, the hardening script for immutable Fedora"
