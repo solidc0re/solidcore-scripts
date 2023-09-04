c4c674e61dc5247f904ace443e30da8978cf5044
solidc0re <140627847+solidc0re@users.noreply.github.com>
2023-09-04 13:15:39 +0100

    consolidated malicious blocklist

--- solidcore-firstboot.sh
+++ solidcore-firstboot.sh
@@ -829,51 +829,31 @@ https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
 
 # === MALICIOUS ===
 # Green lists from firebog.net, the rest from https://github.com/PeterDaveHello/threat-hostlist and https://github.com/hagezi/dns-blocklists#tif
-# Probably overkill?
 https://azorult-tracker.net/api/list/domain?format=plain
 https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt
 https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt
 https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt
 https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt
 https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt
-https://dsi.ut-capitole.fr/blacklists/download/malware.tar.gz
-https://dsi.ut-capitole.fr/blacklists/download/phishing.tar.gz
 https://github.com/DandelionSprout/adfilt/raw/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
 https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/wildcard/tif-onlydomains.txt
 https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt
-https://gitlab.com/ZeroDot1/CoinBlockerLists/-/raw/master/hosts
 https://hole.cert.pl/domains/domains.txt
-https://kriskintel.com/feeds/ktip_covid_domains.txt
-https://kriskintel.com/feeds/ktip_malicious_domains.txt
-https://kriskintel.com/feeds/ktip_ransomware_feeds.txt
 https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt
 https://openphish.com/feed.txt
-https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt
-https://phishing.army/download/phishing_army_blocklist_extended.txt
 https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts
-https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
 https://raw.githubusercontent.com/durablenapkin/scamblocklist/master/hosts.txt
 https://raw.githubusercontent.com/elliotwutingfeng/GlobalAntiScamOrg-blocklist/main/global-anti-scam-org-scam-urls-pihole.txt
 https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
 https://raw.githubusercontent.com/FiltersHeroes/KADhosts/master/KADhosts.txt
-https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hosts.txt
 https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
 https://raw.githubusercontent.com/MetaMask/eth-phishing-detect/master/src/hosts.txt
-https://raw.githubusercontent.com/mitchellkrogza/Badd-Boyz-Hosts/master/hosts
-https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-domains-ACTIVE.txt
 https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt
-https://raw.githubusercontent.com/stamparm/aux/master/maltrail-malware-domains.txt
-https://raw.githubusercontent.com/stamparm/blackbook/master/blackbook.txt
-https://rescure.me/rescure_domain_blacklist.txt
 https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt
 https://securereload.tech/Phishing/Lists/Latest/
-https://threatfox.abuse.ch/downloads/hostfile
-https://urlhaus.abuse.ch/downloads/hostfile/
 https://v.firebog.net/hosts/Prigent-Crypto.txt
 https://v.firebog.net/hosts/RPiList-Malware.txt
 https://v.firebog.net/hosts/RPiList-Phishing.txt
-https://www.stopforumspam.com/downloads/toxic_domains_whole.txt
 https://www.usom.gov.tr/url-list.txt
 
 
